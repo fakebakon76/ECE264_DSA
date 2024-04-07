9ec3d677629075d5ccfdc98752b61bf835269dd9
noamschuck <noam.schuck@gmail.com>
2024-04-07 12:46:11 -0400

    added push functionality

--- lists/main.cpp
+++ lists/main.cpp
@@ -10,10 +10,17 @@
 using namespace std;
 using SimpleListVariant = variant<Queue<int>, Queue<double>, Queue<string>, Stack<int>, Stack<double>, Stack<string>>;
 
+template <typename Object>
+SimpleList<Object> *retrieveList(string name, unordered_map<string, SimpleListVariant> *map);
+
 int createLists(const string file, unordered_map<string, SimpleListVariant> *map);
 int create(string name, string listType, unordered_map<string, SimpleListVariant> *map);
-int push(string name, string value, unordered_map<string, SimpleListVariant> *map);
-int pop(string name);
+
+template <typename Object>
+int push(string name, Object value, unordered_map<string, SimpleListVariant> *map);
+
+template <typename Object>
+int pop(string name, unordered_map<string, SimpleListVariant> *map);
 
 int main() {
     /*
@@ -59,10 +66,16 @@ int createLists(const string file, unordered_map<string, SimpleListVariant> *map
         
         iss >> command >> name >> third;
         //cout << command << " " << name << " " << third <<"\n";
+        char type = name[0];
         if(command == "push") {
-            push(name, third, map);
+            if(type == 'i')      push<int>(name, stoi(third), map);
+            else if(type == 'd') push<double>(name, stod(third), map);
+            else if(type == 's') push<string>(name, third, map);
         }
         else if (command == "pop") {
+            if(type == 'i')      pop<int>(name, map);
+            else if(type == 'd') pop<double>(name, map);
+            else if(type == 's') pop<string>(name, map);
         }
         else if (command == "create") {
             create(name, third, map);
@@ -102,7 +115,8 @@ int create(string name, string listType, unordered_map<string, SimpleListVariant
     return 0;
 }
 
-int push(string name, string value, unordered_map<string, SimpleListVariant> *map) {
+template <typename Object>
+int push(string name, Object value, unordered_map<string, SimpleListVariant> *map) {
     cout << "PROCESSING COMMAND: push " << name << " " << value << "\n";
     
     if(! map->count(name)) {
@@ -110,31 +124,25 @@ int push(string name, string value, unordered_map<string, SimpleListVariant> *ma
         return -1;
     }
 
-    char type = name[0];
-
-    if(type == 'i') {
-        SimpleList<int> *list;
-        list = get_if<Stack<int>>(&(map->at(name)));
-        if(list == nullptr) list = get_if<Queue<int>>(&(map->at(name)));
-        list->push(stoi(value));
-    }
-    else if(type == 'd') {
-        SimpleList<double> *list;
-        list = get_if<Stack<double>>(&(map->at(name)));
-        if(list == nullptr) list = get_if<Queue<double>>(&(map->at(name)));
-        list->push(stod(value));
-    }
-    else if(type == 's') {
-        SimpleList<string> *list;
-        list = get_if<Stack<string>>(&(map->at(name)));
-        if(list == nullptr) list = get_if<Queue<string>>(&(map->at(name)));
-        list->push(value);
-    }
-    
+    SimpleList<Object> *list = retrieveList<Object>(name, map);
+    list->push(value);
+    list->traverse();
     return 0;
 }
-
-int pop(string name) {
+/*
+template <typename Object>
+int pop(string name, unordered_map<string, SimpleListVariant> *map) {
     cout << "PROCESSING COMMAND: pop " << name << "\n";
+
+    SimpleList<Object> *list = retrieveList<Object>(name, map);
+    cout << "Value popped: " << list->pop() << "\n";
+
     return 0;
+}*/
+
+template <typename Object>
+SimpleList<Object> *retrieveList(string name, unordered_map<string, SimpleListVariant> *map) {
+    SimpleList<Object> *list = get_if<Stack<Object>>(&(map->at(name)));
+    if(list == nullptr) list = get_if<Queue<Object>>(&(map->at(name)));
+    return list;
 }
